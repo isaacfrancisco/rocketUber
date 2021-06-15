ca9b56d5535529831093e8b7411cedce74187917
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-06-15 14:47:33 -0300

    feat: add MapView with user location.
    solves task card #F0005.

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View } from "react-native";
+import { css } from "./assets/Css/Css";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import { LOCATION_BACKGROUND } from "expo-permissions";
 
 export default function App() {
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+
+  useEffect(() => {
+    (async function () {
+      const { status, permissions } = await Permissions.askAsync(
+        Permissions.LOCATION_BACKGROUND
+      );
+      if (status === "granted") {
+        let location = await Location.getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+        setOrigin({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.00922,
+          longitudeDelta: 0.00421,
+        });
+      } else {
+        throw new Error("Location permission not granted");
+      }
+    })();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={css.container}>
+      <MapView
+        style={css.map}
+        initialRegion={origin}
+        showsUserLocation={true}
+      ></MapView>
+      <View style={css.search}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ assets/Css/Css.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const css = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  map: {
+    height: "60%",
+    backgroundColor: "black",
+  },
+  search: {
+    height: "40%",
+    backgroundColor: "gray",
+  },
+});
+
+export { css };
